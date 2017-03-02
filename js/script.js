c2a8750e8d8ebf6b8490860547dacf8d58187c9d
kuznetsov_e <jackk@yandex.ru>
2017-03-02 22:46:52 +0300

    Model started.

--- js/script.js
+++ js/script.js
@@ -11,4 +11,17 @@ let view = {
         let cell = document.getElementById(location);
         cell.setAttribute("class", "miss");
     }
+};
+
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+    ships: [
+        {location: ["06", "16", "26"], hits: ["", "", ""]},
+        {location: ["24", "34", "44"], hits: ["", "", ""]},
+        {location: ["10", "11", "12"], hits: ["", "", ""]}
+    ]
 }
